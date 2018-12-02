672e4a542539eee042a9318982eed9afc9cb4a5d
basilkar <bkaradice@yahoo.com>
2018-12-02 14:51:42 +0100

    Prepare modules

--- fun-notespace/app/Main.hs
+++ fun-notespace/app/Main.hs
@@ -1,6 +1,32 @@
 module Main where
 
+import System.IO
+
+-- third party libs
+import Data.Time (getCurrentTime)
+
+-- local libs
+import Guitar
+import InductiveChords
 import Lib
+import Notes
+import Rhythm
+import Tymoczko
 
 main :: IO ()
-main = someFunc
+main = do
+    putStrLn "Notes' Pace (yet another music app)"
+    todo
+
+todo :: IO ()
+todo = do
+    time <- getCurrentTime
+    putStrLn (show time)
+    putStrLn "TODO:"
+    putStrLn "1. organize the haskell part of the project"
+    putStrLn "2. finish implementing Tymoczko's stuff"
+
+printConfig :: IO ()
+printConfig = do
+    contents <- readFile "stack.yaml"
+    putStrLn contents
---
+++ fun-notespace/src/Guitar.hs
@@ -0,0 +1,148 @@
+module Guitar where
+
+{------------------------------}
+{- 5 -- CONCERNING THE GUITAR -}
+{------------------------------}
+
+{-
+Watching this guy here, https://www.youtube.com/watch?v=b9pYEjZ4l48, it seemed reasonable to start implementing some of the quirks that are particular to the guitar.
+-}
+
+-- Start by introducing two basic datatypes, one for the strings and one for the frets.
+
+data Guitarstring = StringOne | StringTwo | StringThree | StringFour | StringFive | StringSix
+  deriving (Eq, Enum, Ord, Show, Read, Bounded)
+
+data Fret = FretZero | FretOne | FretTwo | FretThree | FretFour | FretFive | FretSix | FretSeven | FretEight | FretNine | FretTen | FretEleven | FretTwelve
+  deriving (Eq, Enum, Ord, Show, Read, Bounded)
+
+-- Then a couple of the common tunings.
+
+tuningStandard :: Guitarstring -> Note
+tuningStandard string = case string of
+  StringOne -> E
+  StringTwo -> B
+  StringThree -> G
+  StringFour -> D
+  StringFive -> A
+  StringSix -> E
+
+tuningDropD :: Guitarstring -> Note
+tuningDropD string = case string of
+  StringOne -> E
+  StringTwo -> B
+  StringThree -> G
+  StringFour -> D
+  StringFive -> A
+  StringSix -> D
+
+tuningOpenD :: Guitarstring -> Note
+tuningOpenD string = case string of
+  StringOne -> D
+  StringTwo -> A
+  StringThree -> Fs
+  StringFour -> D
+  StringFive -> A
+  StringSix -> D
+
+tuningDadgad :: Guitarstring -> Note
+tuningDadgad string = case string of
+  StringOne -> D
+  StringTwo -> A
+  StringThree -> G
+  StringFour -> D
+  StringFive -> A
+  StringSix -> D
+
+tuningNST :: Guitarstring -> Note
+tuningNST string = case string of
+  StringOne -> G
+  StringTwo -> E
+  StringThree -> A
+  StringFour -> D
+  StringFive -> G
+  StringSix -> C
+
+-- Just a grain of sugar.
+
+type Tuning = Guitarstring -> Note
+
+-- And now, the fretboard; given a tuning, we allot notes to the pairs of strings and frets.
+
+fretboard :: Tuning -> Guitarstring -> Fret -> Note
+fretboard tuning string fret = halfsteps (tuning string) (fromEnum fret)
+
+{-----------------------------------------}
+{- 5.1 -- APPLICATION: FRETBOARD QUIZZES -}
+{-----------------------------------------}
+
+{-
+We can now implement the quizzes that the youtube guy from above suggested. For this, we will need to use the IO monad. We will also need the Random module. See https://wiki.haskell.org/Introduction_to_IO, https://wiki.haskell.org/IO_inside and  https://www.schoolofhaskell.com/school/starting-with-haskell/libraries-and-frameworks/randoms.
+-}
+
+-- To be able to generate random strings, frets, or notes for the needs of the quiz, we need to make the respective types into instances of the class Random.
+
+instance Random Guitarstring where
+  randomR (string,string') generator = case randomR (fromEnum string, fromEnum string') generator of
+    (stringNumber, generator') -> (toEnum stringNumber, generator')
+  random generator = randomR (minBound,maxBound) generator
+
+instance Random Fret where
+  randomR (fret,fret') generator = case randomR (fromEnum fret, fromEnum fret') generator of
+    (fretNumber, generator') -> (toEnum fretNumber, generator')
+  random generator = randomR (minBound,maxBound) generator
+
+instance Random Note where
+  randomR (note,note') generator = case randomR (fromEnum note, fromEnum note') generator of
+    (noteNumber, generator') -> (toEnum noteNumber, generator')
+  random generator = randomR (minBound,maxBound) generator
+
+-- Now we can define the quizzes as follows.
+
+fretboardQuizOneStandard :: IO ()
+fretboardQuizOneStandard = do
+  g1 <- newStdGen
+  g2 <- newStdGen
+  let fret = head (randoms g1 :: [Fret])
+  let string = head (randoms g2 :: [Guitarstring])
+  let answer = fretboard tuningStandard string fret
+  putStrLn ("What is the note on fret " ++ (show $ fromEnum fret) ++ " of string " ++ (show $ (fromEnum string) + 1) ++ " in standard tuning?")
+  guess <- getLine
+  if guess == (show answer) then putStr "Correct!" else putStr ("Wrong! The note is actually " ++ (show answer) ++ ".")
+
+fretboardQuizTwoStandard :: IO ()
+fretboardQuizTwoStandard = do
+  g1 <- newStdGen
+  g2 <- newStdGen
+  let note = head (randoms g1 :: [Note])
+  let string = head (randoms g2 :: [Guitarstring])
+  putStrLn ("Where is the note " ++ (show note) ++ " on string " ++ (show $ (fromEnum string) + 1) ++ " in standard tuning?")
+  guess <- readLn --getLine
+  if (fretboard tuningStandard string (toEnum guess)) == note then putStr ("Correct!") else putStr ("Wrong.") -- Needs some more work to also provide the user with the correct answer here.
+
+-- For the following version see https://stackoverflow.com/a/35639761/3749908.
+
+main :: IO ()
+main = do
+  let loop = do
+      g <- newStdGen
+      let b = head (randoms g :: [Bool])
+      if b then (fretboardQuizOneStandard) else (fretboardQuizTwoStandard)
+      putStrLn (" Hit enter to continue or enter an arbitrary key to exit.")
+      usersays <- getLine
+      when (usersays == "") loop
+  loop
+
+-- This works already:
+
+-- main :: IO ()
+-- main = do
+--   g <- newStdGen
+--   let b = head (randoms g :: [Bool])
+--   if b then (fretboardQuizOneStandard) else (fretboardQuizTwoStandard)
+
+-- In an exactly similar manner we may define quizzes for other tunings. Of course, it'd be even better to let the user decide which tuning they want to be quized on. Ma non adesso.
+
+-- while :: Bool -> IO () -> IO ()
+-- while False _ = return ()
+-- while True a = do a
