11b7f1e7ac59540c27817ef1931b069cb14adf77
basilkar <bkaradice@yahoo.com>
2019-01-27 15:24:16 +0100

    Comment Math.Combinat out
    It can't import, haven't looked into why yet.

--- fun-notespace/src/haskell-prototyping.hs
+++ fun-notespace/src/haskell-prototyping.hs
@@ -4,7 +4,7 @@
 
 import Data.List -- for sorting lists, for nub
 import Data.Array -- for the memoization in the definition of Levenshtein metric
-import Math.Combinat -- for rhythmic exercises
+-- import Math.Combinat -- for rhythmic exercises
 import System.Random -- for the guitar fretboard quizzes
 import Control.Monad -- for the loop-while in the main quiz
 
@@ -1102,29 +1102,29 @@ ppcL as = map ppc as
 
 -}
 
-{---------------------}
-{- 3 -- ABOUT RHYTHM -}
-{---------------------}
-
--- We may view time signatures like 4/4, 3/4, et cetera, as pairs of integers.
-
-type TimeSig = (Int,Int) -- well, the integers are supposed to be positive
-
--- Given a time signature and a number of bars, give all rhythmic patterns for the whole span of bars.
-
-rhythmPatterns :: (Int,Int) -> Int -> [[Bool]]
-rhythmPatterns tsig b = [map (\ x -> elem x (numPatterns !! i)) allBeats | i <- [0..(len2-1)]] {-[map (\ x -> elem x (numPatterns !! i)) allBeats | i <- [0..len]]-} {-[ [elem x (numPatterns !! i) | x <- allBeats] | i <- [0..len]]-}
-    where
-        len = length allBeats
-        numPatterns = concat [choose i allBeats | i <- [0..len]]
-        allBeats = [1..(b * (fst tsig))]
-        len2 = length numPatterns
-
--- The following yields all rhythm patterns that stress exactly m many beats of all
-
-rhythmPatternsWith :: (Int, Int) -> Int -> Int -> [[Bool]]
-rhythmPatternsWith tsig b m = filter ([False | i <- [1..((b*(fst tsig)) - m)]] `isSubsequenceOf`) $ filter ([True | i <- [1..m]] `isSubsequenceOf` ) $ rpats
-    where rpats = rhythmPatterns tsig b
+-- {---------------------}
+-- {- 3 -- ABOUT RHYTHM -}
+-- {---------------------}
+-- 
+-- -- We may view time signatures like 4/4, 3/4, et cetera, as pairs of integers.
+-- 
+-- type TimeSig = (Int,Int) -- well, the integers are supposed to be positive
+-- 
+-- -- Given a time signature and a number of bars, give all rhythmic patterns for the whole span of bars.
+-- 
+-- rhythmPatterns :: (Int,Int) -> Int -> [[Bool]]
+-- rhythmPatterns tsig b = [map (\ x -> elem x (numPatterns !! i)) allBeats | i <- [0..(len2-1)]] {-[map (\ x -> elem x (numPatterns !! i)) allBeats | i <- [0..len]]-} {-[ [elem x (numPatterns !! i) | x <- allBeats] | i <- [0..len]]-}
+--     where
+--         len = length allBeats
+--         numPatterns = concat [choose i allBeats | i <- [0..len]]
+--         allBeats = [1..(b * (fst tsig))]
+--         len2 = length numPatterns
+-- 
+-- -- The following yields all rhythm patterns that stress exactly m many beats of all
+-- 
+-- rhythmPatternsWith :: (Int, Int) -> Int -> Int -> [[Bool]]
+-- rhythmPatternsWith tsig b m = filter ([False | i <- [1..((b*(fst tsig)) - m)]] `isSubsequenceOf`) $ filter ([True | i <- [1..m]] `isSubsequenceOf` ) $ rpats
+--     where rpats = rhythmPatterns tsig b
 
 {- EXAMPLES -}
 
