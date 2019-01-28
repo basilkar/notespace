edf8ec17495c03c70bce446d577f4bacf2fb5202
Brent <jbrentrunyan@gmail.com>
2019-01-28 20:30:34 +0100

    Added a little style and some basic mingus usage.

--- HarmonyHints/main.py
+++ HarmonyHints/main.py
@@ -1,41 +1,84 @@
-from PyQt5.QtWidgets import (QApplication, QMainWindow, QWidget, QTextEdit, QLabel, QVBoxLayout, QHBoxLayout, QComboBox, QPushButton)
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QIcon
+
+from style import darkgrey_css
+
+import mingus.core.keys as mingkeys
+import mingus.core.scales as mingscales
 
 
+def keys():
+    return list(map(lambda x: x[0], mingkeys.keys))
+
+
+def scaleNotes(key, scale):
+    if scale == 'Chromatic':
+        return mingscales.Chromatic(key).ascending()
+    elif scale == 'Whole Tone':
+        return mingscales.WholeTone(key).ascending()
+    elif scale == 'Octatonic':
+        return mingscales.Octatonic(key).ascending()
+    else:
+        return []
 
 app = QApplication([])
-app.setApplicationName("Harmony Hints")
+app.setApplicationName("Arm me with harmony.")
 
 mainLayout = QVBoxLayout()
 
 layout0 = QHBoxLayout()
 
-startChord = QTextEdit("C#")
-endChord = QTextEdit("Ebm")
+textArea = QTextEdit()
 
-chordTransitions = QTextEdit()
+keysComboBox = QComboBox()
+keysComboBox.addItems(keys())
+scalesComboBox = QComboBox()
+scalesComboBox.addItems(['Chromatic', 'Whole Tone', 'Octatonic'])
 
-def foo(checked):
-    print("pressed: ", startChord.toPlainText())
-    transition = startChord.toPlainText() + " -> " + endChord.toPlainText() 
-    chordTransitions.setText(transition)
 
-layout0.addWidget(startChord)
-layout0.addWidget(endChord)
+def setScale(scale):
+    key = keysComboBox.currentText()
+    textArea.setText(' - '.join(scaleNotes(key, scale)))
+
+scalesComboBox.currentTextChanged.connect(setScale)
+
+
+def setKey(key):
+    scale = scalesComboBox.currentText()
+    textArea.setText(' - '.join(scaleNotes(key, scale)))
+
+keysComboBox.currentTextChanged.connect(setKey)
+
+layout0.addWidget(QLabel('Key: '))
+layout0.addWidget(keysComboBox)
+layout0.addWidget(QLabel('Scale: '))
+layout0.addWidget(scalesComboBox)
+layout0.setStretch(0, 1)
+layout0.setStretch(1, 1)
+layout0.setStretch(2, 1)
+layout0.setStretch(3, 3)
 
 runButton = QPushButton("Run")
-
-runButton.clicked.connect(foo)
+runButton.clicked.connect(lambda checked: print(keys()))
 
 mainLayout.addLayout(layout0)
-mainLayout.addWidget(chordTransitions)
-mainLayout.addWidget(runButton)
+mainLayout.addWidget(textArea)
+# mainLayout.addWidget(runButton)
+
+setKey(keysComboBox.currentText())
 
 w = QWidget()
-w.resize(512, 512)
 w.setLayout(mainLayout)
 
 mainWindow = QMainWindow()
 mainWindow.setCentralWidget(w)
+mainWindow.resize(1200, 200)
 mainWindow.show()
 
+app.setStyleSheet(darkgrey_css)
+
+font = app.font()
+font.setPointSize(32)
+app.setFont(font)
+app.setWindowIcon(QIcon('HH.png'))
 app.exec_()
